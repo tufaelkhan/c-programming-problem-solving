e40b5a22ac9ab641425df1e6316bb3b89a0b8866
tufaelkhan <tufaelkhan247@gmail.com>
2024-07-05 11:43:18 +0600

    add builtin function

--- module-ten/function_4.c
+++ module-ten/function_4.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
+void sum(void){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int total = a+b;
+    printf("%d\n",total);
+}
+
 int main(){
-    
+    sum();
+    sum();
     return 0;
 }
---
+++ module-ten/math_function.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main(){
+    // double x;
+    // scanf("%lf",&x);
+    // int ans = ceil(x);
+    // int ans = floor(x);
+    // int ans = round(x);
+
+    // double x;
+    // scanf("%lf",&x);
+    // double ans = sqrt(x);
+    // printf("%0.2lf\n",ans);  
+
+    // double a,b;
+    // scanf("%lf %lf",&a,&b);
+    // double ans = pow(a,b);
+    // printf("%0.2lf\n",ans);
+
+    int x;
+    scanf("%d",&x);
+    int ans = abs(x);
+    printf("%d\n",ans);
+    return 0;
+}
